b167510f41e1a999ae1cfdd1b0ec9f5c7acd2218
Richard Tape <richard@iamfriendly.com>
2013-01-30 11:34:16 +0000

    Refactored plugin, better comments, file_exists checks, LOCAL_DEV definition for plugins_url filter

--- chemistrycodehighlighter.php
+++ chemistrycodehighlighter.php
@@ -1,87 +1,134 @@
 <?php
-/*
-Plugin Name: Themeists Chemistry Code Highlighter Potion
-Plugin URI: #
-Description:This plugin adds a code highligter to your Chemistry editor. It allows you to show off some great code snippets in your Chemistry templates
-Version: 1.0
-Author: Richard Tape
-Author URI: #
-License: GPL2
-*/
 
-if( !class_exists( 'ChemistryCodeHighlighter' ) ):
+	/*
+	Plugin Name: Themeists Chemistry Code Highlighter Potion
+	Plugin URI: http://themeists.com/plugins/chemistry-code-highlighter/
+	Description:This plugin adds a code highligter to your Chemistry editor. It allows you to show off some great code snippets in your Chemistry templates
+	Version: 1.0
+	Author: Richard Tape
+	Author URI: http://themeists.com/
+	License: GPL2
+	*/
 
-
-	/**
-	 * Adds an extra potion to the Chemistry editor for highlighting code.
-	 *
-	 * @author Richard Tape
-	 * @package ChemistryCodeHighlighter
-	 * @since 1.0
-	 */
-	
-	class ChemistryCodeHighlighter
-	{
-
+	if( !class_exists( 'ChemistryCodeHighlighter' ) ):
 
 		/**
-		 * We might not be using a themeists theme (which means we can't add anything to the options panel). By default,
-		 * we'll say we are not. We check if the theme's author is Themeists to set this to true during instantiation.
+		 * Adds an extra potion to the Chemistry editor for highlighting code.
 		 *
 		 * @author Richard Tape
 		 * @package ChemistryCodeHighlighter
 		 * @since 1.0
 		 */
 		
-		var $using_themeists_theme = false;
-		
-
-		/**
-		 * Initialise ourselves and do a bit of setup
-		 *
-		 * @author Richard Tape
-		 * @package ChemistryCodeHighlighter
-		 * @since 1.0
-		 * @param None
-		 * @return None
-		 */
-
-		function ChemistryCodeHighlighter()
+		class ChemistryCodeHighlighter
 		{
 
-			$theme_data = wp_get_theme();
-			$theme_author = $theme_data->display( 'Author', false );
+			/**
+			 * We might not be using a themeists theme (which means we can't add anything to the options panel).
+			 * By default, we'll say we are not. We check if the theme's author is Themeists to set this to
+			 * true during instantiation.
+			 *
+			 * @author Richard Tape
+			 * @package ChemistryCodeHighlighter
+			 * @since 1.0
+			 */
+			
+			var $using_themeists_theme = false;
+			
 
-			if( strtolower( trim( $theme_author ) ) == "themeists" )
-				$this->using_themeists_theme = true;
+			/**
+			 * Initialise ourselves and do a bit of setup. We need to hook into chemistry_external_potion
+			 * to be able to add this new potion to our potions array
+			 *
+			 * @author Richard Tape
+			 * @package ChemistryCodeHighlighter
+			 * @since 1.0
+			 * @param None
+			 * @return None
+			 */
 
-			if( $this->using_themeists_theme )
-				add_action( 'chemistry_external_potions', 	array( &$this, 'load_new_potion' ), 10, 1 );
+			function ChemistryCodeHighlighter()
+			{
 
+				//We only load potions if we're using a Themeists theme
+				$theme_data = wp_get_theme();
+				$theme_author = $theme_data->display( 'Author', false );
 
-		}/* ChemistryCodeHighlighter() */
+				//Check the author name
+				if( strtolower( trim( $theme_author ) ) == "themeists" )
+					$this->using_themeists_theme = true;
 
-		
-
-		function load_new_potion()
-		{
-
-			$potion = 'chemistry_potion_code_highlighter';
-			$potion_path = plugin_dir_path( __FILE__ ) . 'class.' . $potion . '.php';
-
-			require_once( $potion_path );
-
-			//we're loaded, so instantiate
-			if( class_exists( 'chemistry_molecule' ) && class_exists( $potion ) )
-				chemistry_molecule::use_potion( $potion );
-
-		}/* load_new_potion() */
-
-		
-	}/* class ChemistryCodeHighlighter */
-
-endif;
+				//If we're on a themeists theme, load the potion
+				if( $this->using_themeists_theme )
+					add_action( 'chemistry_external_potions', 	array( &$this, 'load_new_potion' ), 10, 1 );
 
 
-//And so it begins
-$chemistry_code_highlighter = new ChemistryCodeHighlighter;
+			}/* ChemistryCodeHighlighter() */
+
+			
+			/**
+			 * Load potion class from this plugin. First we load the potion class which acts similarly
+			 * to a widget. Then pass that potion class to chemistry_molecule::use_potion()
+			 *
+			 * @author Richard Tape
+			 * @package ChemistryCodeHighlighter
+			 * @since 1.0
+			 * @param None
+			 * @return Uses use_potion()
+			 */
+			
+			function load_new_potion()
+			{
+
+				//Set the potion class name and form the path to the class file
+				$potion = 'chemistry_potion_code_highlighter';
+				$potion_path = plugin_dir_path( __FILE__ ) . 'class.' . $potion . '.php';
+
+				//If that file name exists, load it
+				if( file_exists( $potion_path ) )
+					require_once( $potion_path );
+
+				//we're loaded, so instantiate
+				if( class_exists( 'chemistry_molecule' ) && class_exists( $potion ) )
+					chemistry_molecule::use_potion( $potion );
+
+			}/* load_new_potion() */
+
+			
+		}/* class ChemistryCodeHighlighter */
+
+	endif;
+
+
+	//And so it begins
+	$chemistry_code_highlighter = new ChemistryCodeHighlighter;
+
+	/*
+
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+	@@                                                   @@
+	@@                                                   @@
+	@@                                                   @@
+	@@                                                   @@
+	@@                                                   @@
+	@@                                                   @@
+	@@                                                   @@
+	@@                                                   @@
+	@@    @@@@@@@@@@@@                                   @@
+	@@    @@@@@@@@@@@@                                   @@
+	@@    ++++#@@@++++                                   @@
+	@@        ;@@+         ,     .                       @@
+	@@        ;@@+   +@@ @@@@@ @@@@@                     @@
+	@@        ;@@+   +@@@@@@@@@@@@@@@                    @@
+	@@        ;@@+   +@@@  `@@@   @@@                    @@
+	@@        ;@@+   +@@    @@@   @@@                    @@
+	@@        ;@@+   +@@    @@@   @@@                    @@
+	@@        ;@@+   +@@    @@@   @@@                    @@
+	@@        ;@@+   +@@    @@@   @@@                    @@
+	@@        ;@@+   +@@    @@@   @@@                    @@
+	@@        ;@@+   +@@    @@@   @@@                    @@
+	@@        ;@@+   +@@    @@@   @@@                    @@
+	@@                                                   @@
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+	*/
--- class.chemistry_potion_code_highlighter.php
+++ class.chemistry_potion_code_highlighter.php
@@ -9,15 +9,15 @@
 			/**
 			 * Register this potion. It will add it to the overlay. If the 1st parameter that
 			 * you pass to it's parent's __constructor() method is "example" then it will create
-			 * a div with a class of "molecule-widget-type-example" as a wrapper and then another div
-			 * with a class of potion-code. You should style the .molecule-widget-bar with your
+			 * a div with a class of "potion-type-example" as a wrapper and then another div
+			 * with a class of potion-example. You should style the .chemistry-potion-inner with your
 			 * appropriate icon, i.e.
 			 *
-			 * #molecule-widgets .molecule-widget-type-example .molecule-widget-bar{ styles_here }
+			 * #potions-overlay .potion-type-example .chemistry-potion-inner{ styles_here }
 			 *
 			 * @author Richard Tape
-			 * @package Chemistry
-			 * @since 0.7
+			 * @package chemistry_potion_code_highlighter
+			 * @since 1.0
 			 * @param None
 			 * @return None
 			 */
@@ -29,19 +29,21 @@
 				$this->label = __( 'An easy-to-use syntax highgter for sharing code.' , 'chemistry' );
 
 				//we need to add a filter to plugins_url as we use symlinks in our dev setup
-				add_filter( 'plugins_url', array( &$this, 'local_dev_symlink_plugins_url_fix' ), 10, 3 );
+				if( defined( 'LOCAL_DEV' ) && LOCAL_DEV )
+					add_filter( 'plugins_url', array( &$this, 'local_dev_symlink_plugins_url_fix' ), 10, 3 );
 
 			}/* __construct() */
 
 
 			/* =========================================================================== */
 			
+
 			/**
-			 * Out the markup for our code highlighter - ensure we load our styles and scripts first
+			 * Output the markup for our code highlighter - ensure we load our styles and scripts first
 			 *
 			 * @author Richard Tape
-			 * @package Chemistry
-			 * @since 0.7
+			 * @package chemistry_potion_code_highlighter
+			 * @since 1.0
 			 * @param (array) $widget - widget config
 			 * @return Markup
 			 */
@@ -49,20 +51,25 @@
 			public function widget( $widget )
 			{
 
-				//Load our necessary scripts and styles
-				wp_enqueue_style( 'shCoreDefault', plugins_url( '_a/css/sh/shCoreDefault.css', __FILE__ ), '', Chemistry::chemistry_option( 'chemistry_version' ) );
+				//Load our necessary scripts and styles. Check for Chemistry so we can get the version
+				if( class_exists( 'Chemistry' ) )
+				{
 
-				wp_enqueue_script( 'shCore', plugins_url( '_a/js/sh/shCore.js', __FILE__ ), '', Chemistry::chemistry_option( 'chemistry_version' ) );
+					wp_enqueue_style( 'shCoreDefault', plugins_url( 'assets/css/sh/shCoreDefault.css', __FILE__ ), '', Chemistry::chemistry_option( 'chemistry_version' ) );
 
-				wp_enqueue_script( 'shAutoloader', plugins_url( '_a/js/sh/shAutoloader.js', __FILE__ ), array( 'shCore' ), Chemistry::chemistry_option( 'chemistry_version' ) );
+					wp_enqueue_script( 'shCore', plugins_url( 'assets/js/sh/shCore.js', __FILE__ ), '', Chemistry::chemistry_option( 'chemistry_version' ) );
 
+					wp_enqueue_script( 'shAutoloader', plugins_url( 'assets/js/sh/shAutoloader.js', __FILE__ ), array( 'shCore' ), Chemistry::chemistry_option( 'chemistry_version' ) );
 
+				}
 
-				//Sanity check
+				//Sanity check to ensure there is some output
 				$code = isset( $widget['code'] ) ? $widget['code'] : '';
 
-				//Where's your head at?
+				//Where's your head at? As this is such a simple example, this is simply some basic markup
 				$return = '<pre class="brush: ' . $widget['type'] . ';">' . htmlspecialchars( $code ) . '</pre>';
+				
+				//Filters as always
 				return apply_filters( 'chemistry_potion_code_highlighter_markup', $return );
 
 			}/* widget() */
@@ -74,8 +81,8 @@
 			 * The admin form for this widget
 			 *
 			 * @author Richard Tape
-			 * @package Chemistry
-			 * @since 0.7
+			 * @package chemistry_potion_code_highlighter
+			 * @since 1.0
 			 * @param (array) $widget - The specific details for this widget
 			 * @return The markup
 			 */
@@ -86,29 +93,31 @@
 				//The code types we support
 				$types = array( 
 
-					'as3' => __( 'AS3' , 'chemistry' ),
-					'applescript' => __( 'Apple Script' , 'chemistry' ),
-					'bash' => __( 'Bash' , 'chemistry' ),
-					'csharp' => __( 'C#' , 'chemistry' ),
-					'coldfusion' => __( 'Cold Fusion' , 'chemistry' ),
-					'cpp' => __( 'C++' , 'chemistry' ),
-					'css' => __( 'CSS' , 'chemistry' ),
-					'diff' => __( 'Diff' , 'chemistry' ),
-					'javascript' => __( 'Java Script' , 'chemistry' ),
-					'java' => __( 'Java' , 'chemistry' ),
-					'perl' => __( 'Perl' , 'chemistry' ),
-					'php' => __( 'PHP' , 'chemistry' ),
-					'plain' => __( 'Plain' , 'chemistry' ),
-					'python' => __( 'Python' , 'chemistry' ),
-					'ruby' => __( 'Ruby' , 'chemistry' ),
-					'sass' => __( 'SASS' , 'chemistry' ),
-					'xml' => __( 'XML' , 'chemistry' )
+					'as3' 			=> __( 'AS3' , 'chemistry' ),
+					'applescript'	=> __( 'Apple Script' , 'chemistry' ),
+					'bash' 			=> __( 'Bash' , 'chemistry' ),
+					'csharp' 		=> __( 'C#' , 'chemistry' ),
+					'coldfusion' 	=> __( 'Cold Fusion' , 'chemistry' ),
+					'cpp' 			=> __( 'C++' , 'chemistry' ),
+					'css' 			=> __( 'CSS' , 'chemistry' ),
+					'diff' 			=> __( 'Diff' , 'chemistry' ),
+					'javascript' 	=> __( 'Java Script' , 'chemistry' ),
+					'java' 			=> __( 'Java' , 'chemistry' ),
+					'perl' 			=> __( 'Perl' , 'chemistry' ),
+					'php' 			=> __( 'PHP' , 'chemistry' ),
+					'plain' 		=> __( 'Plain' , 'chemistry' ),
+					'python' 		=> __( 'Python' , 'chemistry' ),
+					'ruby' 			=> __( 'Ruby' , 'chemistry' ),
+					'sass' 			=> __( 'SASS' , 'chemistry' ),
+					'xml' 			=> __( 'XML' , 'chemistry' )
 
 				 );
 
+				//Run these through a filter so we can register other types externally. Can see other options
+				//http://alexgorbatchev.com/SyntaxHighlighter
 				$types = apply_filters( 'chemistry_code_highlighter_types', $types );
 
-				//Form markup
+				//Form markup for the overlay of our potion. Check out the chemistry docs to see how to form this
 				$return = '<fieldset class="chemistry-form">
 					<h2 class="chemistry-tab-title">' . __( 'General' , 'chemistry' ) . '</h2>
 					<div class="chemistry-tab-content">
@@ -117,12 +126,23 @@
 					</div>
 				</fieldset>';
 
-				return apply_filters( 'chemistry_code_highlighter_markup', $return );
+				return $return;
 
 			}/* form() */
 
 			/* =========================================================================== */
 
+			/**
+			 * We use symlinks for our local dev (and on our staging site) so we need to filter
+			 * plugins_url()
+			 *
+			 * @author Richard Tape
+			 * @package chemistry_potion_code_highlighter
+			 * @since 1.0
+			 * @param 
+			 * @return 
+			 */
+			
 			function local_dev_symlink_plugins_url_fix( $url, $path, $plugin )
 			{
 
